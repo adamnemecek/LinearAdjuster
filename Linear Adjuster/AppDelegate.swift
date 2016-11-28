bd93216b369ce0d5e6a42c303871124e8ddfb549
Kunio Sawatani <devel@fathens.org>
2016-11-28 17:45:47 +0900

    Initial Commit

---
+++ Linear Adjuster/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Linear Adjuster
+//
+//  Created by 沢谷邦夫 on 11/28/16.
+//  Copyright © 2016 沢谷邦夫. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Linear Adjuster/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Linear Adjuster
+//
+//  Created by 沢谷邦夫 on 11/28/16.
+//  Copyright © 2016 沢谷邦夫. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
