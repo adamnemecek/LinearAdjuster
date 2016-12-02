f0166e809964c8b2c07055b77f8eda278d1ffe0e
Kunio Sawatani <devel@fathens.org>
2016-12-02 15:37:15 +0900

    CATransform3D は新しいレイヤーに適用
    CALayer.transform にセットするとその時点の変形にされに変形をかけてしまいます。
    クリアする方法が見つからなかったので、都度、新しいサブレイヤーを作成して変形することにしました。

--- Linear Adjuster/MtrixView.swift
+++ Linear Adjuster/MtrixView.swift
@@ -8,46 +8,51 @@
 
 import Cocoa
 
-class MtrixView: NSView {
-    private let unit = CGFloat(100.0)
-    private var currentState = ViewState.identity
+fileprivate let unit = CGFloat(100.0)
 
-    override func draw(_ dirtyRect: NSRect) {
-        super.draw(dirtyRect)
+class MtrixView: NSView, CALayerDelegate {
 
-        // Drawing code here.
-        NSColor.white.setFill()
-        NSRectFill(bounds)
-        
+    override func awakeFromNib() {
+        update(viewState: ViewState.identity)
+    }
+
+    func update(viewState: ViewState) {
+        log.debug("Updating \(viewState)")
         if let layer = self.layer {
-            currentState.transform(layer: layer)
-        }
-        
-        let startX = dirtyRect.minX
-        let startY = dirtyRect.minY
-        let endX = dirtyRect.maxX
-        let endY = dirtyRect.maxY
-        
-        for i in 0...Int(dirtyRect.width / unit) {
-            let x = CGFloat(i) * unit
-            let path = NSBezierPath()
-            path.move(to: NSPoint(x: x, y: startY))
-            path.line(to: NSPoint(x: x, y: endY))
-            path.lineWidth = 1
-            path.stroke()
-        }
-        for i in 0...Int(dirtyRect.height / unit) {
-            let y = CGFloat(i) * unit
-            let path = NSBezierPath()
-            path.move(to: NSPoint(x: startX, y: y))
-            path.line(to: NSPoint(x: endX, y: y))
-            path.lineWidth = 1
-            path.stroke()
+            let sub = CALayer()
+            sub.bounds = NSRect(x: 0, y: 0, width: 1000, height: 1000)
+            sub.backgroundColor = NSColor.white.cgColor
+            sub.position = NSPoint(x: 500, y: 500)
+            sub.delegate = self
+            
+            viewState.transform(layer: sub)
+            sub.setNeedsDisplay()
+            
+            layer.sublayers = nil
+            layer.addSublayer(sub)
         }
     }
-    
-    func update(viewState: ViewState) {
-        currentState = viewState
-        needsDisplay = true
+
+    func draw(_ layer: CALayer, in ctx: CGContext) {
+        log.info("Drawing Layer \(layer)")
+        
+        let rect = ctx.boundingBoxOfClipPath
+        
+        let start = NSPoint(x: rect.minX, y: rect.minY)
+        let end = NSPoint(x: rect.maxX, y: rect.maxY)
+        
+        ctx.setLineWidth(1)
+        
+        for i in 0...Int(rect.width / unit) {
+            let x = CGFloat(i) * unit
+            ctx.move(to: NSPoint(x: x, y: start.y))
+            ctx.addLine(to: NSPoint(x: x, y: end.y))
+        }
+        for i in 0...Int(rect.height / unit) {
+            let y = CGFloat(i) * unit
+            ctx.move(to: NSPoint(x: start.x, y: y))
+            ctx.addLine(to: NSPoint(x: end.x, y: y))
+        }
+        ctx.strokePath()
     }
 }
